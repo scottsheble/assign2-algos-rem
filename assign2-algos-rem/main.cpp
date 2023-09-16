e8a2a393be3c02f86f0168a61282446b16aba300
scott sheble <117122862+scottsheble@users.noreply.github.com>
2023-09-16 00:04:40 -0500

    Initial Commit

---
+++ assign2-algos-rem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  assign2-algos-rem
+//
+//  Created by scott sheble on 9/16/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
